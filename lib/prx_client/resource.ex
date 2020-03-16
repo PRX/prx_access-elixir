0b078319251b0802ea8837cd6d6de4a9c985b792
cavis <ryancavis@gmail.com>
2020-03-16 12:25:41 -0600

    Resource nested parsing

--- lib/prx_client/resource.ex
+++ lib/prx_client/resource.ex
@@ -1,8 +1,8 @@
 defmodule PrxClient.Resource do
-  defstruct [:attributes, :_links, :_embedded, :_url, :_status]
+  defstruct [:_status, :_url, :attributes, :_links, :_embedded]
 
   defmodule Link do
-    defstruct [:href, :title, :profile, :templated, :count]
+    defstruct [:href, :title, :profile, :count, templated: false]
 
     def from_json(json) do
       %PrxClient.Resource.Link{
@@ -15,11 +15,22 @@ defmodule PrxClient.Resource do
     end
   end
 
-  def from_json(json) do
+  def build(status, url, body) when status >= 200 and status < 300 do
+    case Poison.decode(body) do
+      {:ok, json} -> {:ok, from_json(status, url, json)}
+      _not_json -> PrxClient.Error.build(status, url, body)
+    end
+  end
+
+  def build(status, url, body), do: PrxClient.Error.build(status, url, body)
+
+  def from_json(status, url, json) do
     %PrxClient.Resource{}
+    |> Map.put(:_status, status)
+    |> Map.put(:_url, url)
     |> Map.put(:attributes, parse_attributes(json))
     |> Map.put(:_links, parse_links(json))
-    |> Map.put(:_embedded, parse_embedded(json))
+    |> Map.put(:_embedded, parse_embedded(status, url, json))
   end
 
   defp parse_attributes(json) do
@@ -37,7 +48,18 @@ defmodule PrxClient.Resource do
   defp parse_link([link | rest]), do: [Link.from_json(link)] ++ parse_link(rest)
   defp parse_link(link), do: Link.from_json(link)
 
-  defp parse_embedded(json) do
-    Map.get(json, "_embedded", %{})
+  defp parse_embedded(status, url, json) do
+    json
+    |> Map.get("_embedded", %{})
+    |> Enum.map(fn {key, embed} -> {key, parse_embed(status, url, embed)} end)
+    |> Map.new()
   end
+
+  defp parse_embed(_status, _url, []), do: []
+
+  defp parse_embed(status, url, [embed | rest]) do
+    [from_json(status, url, embed)] ++ parse_embed(status, url, rest)
+  end
+
+  defp parse_embed(status, url, embed), do: from_json(status, url, embed)
 end
---
+++ test/prx_client/resource_test.exs
@@ -0,0 +1,110 @@
+defmodule PrxClient.ResourceTest do
+  use ExUnit.Case, async: true
+
+  alias PrxClient.Resource
+  alias PrxClient.Error
+
+  test "has fields" do
+    res = %Resource{}
+    assert res._status == nil
+    assert res._url == nil
+    assert res.attributes == nil
+    assert res._links == nil
+    assert res._embedded == nil
+  end
+
+  test "builds from json" do
+    {:ok, res} = Resource.build(200, "http://some.where", "{\"foo\":\"bar\"}")
+    assert %Resource{} = res
+    assert res._status == 200
+    assert res._url == "http://some.where"
+    assert res.attributes == %{"foo" => "bar"}
+    assert res._links == %{}
+    assert res._embedded == %{}
+  end
+
+  test "handles json decode errors" do
+    {:error, err} = Resource.build(200, "http://some.where", "bad-json")
+    assert %Error{} = err
+    assert err.status == 200
+    assert err.url == "http://some.where"
+    assert err.message == "JSON decode error: bad-json"
+    assert err.json == nil
+  end
+
+  test "handles non-2XX text errors" do
+    {:error, err} = Resource.build(404, "http://some.where", "text error msg")
+    assert %Error{} = err
+    assert err.status == 404
+    assert err.url == "http://some.where"
+    assert err.message == "text error msg"
+    assert err.json == nil
+  end
+
+  test "handles non-2XX json errors" do
+    {:error, err} = Resource.build(500, "http://some.where", "{\"foo\":\"bar\"}")
+    assert %Error{} = err
+    assert err.status == 500
+    assert err.url == "http://some.where"
+    assert err.message == "Got 500 for http://some.where"
+    assert err.json == %{"foo" => "bar"}
+  end
+
+  test "parses links" do
+    {:ok, json} =
+      Poison.encode(%{
+        _links: %{
+          "prx:link" => %{href: "my-href", title: "my-title", profile: "my-profile", count: 123}
+        }
+      })
+
+    {:ok, res} = Resource.build(200, "http://some.where", json)
+    assert %Resource{} = res
+
+    assert res._links == %{
+             "prx:link" => %Resource.Link{
+               href: "my-href",
+               title: "my-title",
+               profile: "my-profile",
+               templated: false,
+               count: 123
+             }
+           }
+  end
+
+  test "parses embedded resources" do
+    {:ok, json} =
+      Poison.encode(%{
+        _embedded: %{
+          "prx:embed" => [
+            %{id: "one"},
+            %{id: "two", _links: %{"prx:link" => %{href: "embedded-link"}}}
+          ]
+        }
+      })
+
+    {:ok, res} = Resource.build(200, "http://some.where", json)
+    assert %Resource{} = res
+
+    assert res._embedded == %{
+             "prx:embed" => [
+               %Resource{
+                 _status: 200,
+                 _url: "http://some.where",
+                 attributes: %{"id" => "one"},
+                 _embedded: %{},
+                 _links: %{}
+               },
+               %Resource{
+                 _status: 200,
+                 _url: "http://some.where",
+                 attributes: %{"id" => "two"},
+                 _embedded: %{},
+                 _links: %{
+                   "prx:link" => %Resource.Link{href: "embedded-link"}
+                 }
+               }
+             ]
+           }
+  end
+end
